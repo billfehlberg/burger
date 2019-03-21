a811dedde5fbe1ce21347b608b65828dcdb5de4a
billfehlberg <wfehlberg72@gmail.com>
2019-03-20 21:58:50 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWDB_URL)  {
-  connection = mysql.createConnection(process.env.JAWDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 var connection = mysql.createConnection({
   host: "localhost",
